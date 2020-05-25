f061e86b6c1ec02e0d640ec07383e22d0810d3b8
gdu199 <gdu199@mail.ru>
2020-05-25 14:45:50 +0300

    переделал после просмотра решений преподавателя

--- Lesson_3/task_6.py
+++ Lesson_3/task_6.py
@@ -4,12 +4,17 @@ def int_func(var_str:str) -> str:
     new_str = var_str[0].upper() + var_str[1:]
     return new_str
 
-print(int_func('text'))
+def int_func2(var_str: str) -> str:
+    return var_str[0].upper() + var_str[1:] if var_str else var_str #''.join(
+
+def user_temp(string: str):
+    return ' '.join(map(int_func2, string.split(' ')))
 
 var_str = input('Введите строку из слов, разделенных пробелом в нижнем регистре.\n').strip()
-my_list = var_str.split(' ')
-new_str = ''
-for el in my_list:
-    new_str = new_str + int_func(el) + ' '
-
-print(new_str.strip())
+print(user_temp(var_str))
+# my_list = var_str.split(' ')
+# new_str = ''
+# for el in my_list:
+#     new_str = new_str + int_func2(el) + ' '
+#
+# print(new_str.strip())
